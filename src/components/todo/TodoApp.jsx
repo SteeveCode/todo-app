ecaabcdb258c6b2a130767c165fc1ee435657998
steevecode <stevecode@yahoo.com>
2023-07-18 15:47:04 +0100

    Step 10 - Displaying More Todo Details in React List Todo Component

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './TodoApp.css'
-import {BrowserRouter, Routes, Route, useNavigate, useParams} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, useNavigate, useParams, Link} from 'react-router-dom'
 
 
 export default function TodoApp() {
@@ -86,7 +86,9 @@ function WelcomeComponent() {
     return (
         <div className="WelcomeComponent">
             <h1>Welcome {username}</h1>
-            Welcome Component
+            <div>
+                Manage your todos - <Link to="/todos">Go here</Link>
+            </div>
         </div>
     )
 }
@@ -103,40 +105,48 @@ function ErrorComponent() {
 }
 
 function ListTodosComponent() {
+    const today = new Date()
+    
+    const targetDate = new Date(today.getFullYear()+12, today.getMonth(), today.getDay())
+    
     const todos = [
-                    {id: 1, description: 'Learn AWS'},
-                    {id: 2, description: 'Learn Full Stack Dev'},
-                    {id: 3, description: 'Learn DevOps'},
+                    {id: 1, description: 'Learn AWS', done: false, targetDate:targetDate},
+                    {id: 2, description: 'Learn Full Stack Dev', done: false, targetDate:targetDate},
+                    {id: 3, description: 'Learn DevOps', done: false, targetDate:targetDate},
                 ]
 
 
-    return (
-        <div className="ListTodosComponent">
-            <h1>Things You Want To Do!</h1>
-            <div>
-                <table>
-                    <thead>
-                            <tr>
-                                <td>id</td>
-                                <td>description</td>
-                            </tr>
-                    </thead>
-                    <tbody>
-                    {
-                        todos.map(
-                            todo => (
-                                <tr key={todo.id}>
-                                    <td>{todo.id}</td>
-                                    <td>{todo.description}</td>
-                                </tr>
-                            )
-                        )
-                    }
-
-                    </tbody>
-
-                </table>
-            </div>
-        </div>
-    )
+                return (
+                    <div className="ListTodosComponent">
+                        <h1>Things You Want To Do!</h1>
+                        <div>
+                            <table>
+                                <thead>
+                                        <tr>
+                                            <td>ID</td>
+                                            <td>Description</td>
+                                            <td>Is Done?</td>
+                                            <td>Target Date</td>
+                                        </tr>
+                                </thead>
+                                <tbody>
+                                {
+                                    todos.map(
+                                        todo => (
+                                            <tr key={todo.id}>
+                                                <td>{todo.id}</td>
+                                                <td>{todo.description}</td>
+                                                <td>{todo.done.toString()}</td>
+                                                <td>{todo.targetDate.toDateString()}</td>
+                                            </tr>
+                                        )
+                                    )
+                                }
+                                </tbody>
+            
+                            </table>
+                        </div>
+                    </div>
+                )
 }
+            
