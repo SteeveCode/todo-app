a3de35efbc3ae8f7128c73e808cd2f9fd7b86342
steevecode <stevecode@yahoo.com>
2023-07-19 14:07:06 +0100

    Step 16 - Updating React State and Verifying Updates through Auth Context

--- src/components/todo/security/AuthContext.js
+++ src/components/todo/security/AuthContext.js
@@ -1,10 +1,12 @@
 
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 
 //1: Create a Context
 
 export const AuthContext = createContext()
+
+export const useAuth = () => useContext(AuthContext)
 
 
 // Share the created contect with other components
@@ -14,6 +16,7 @@ export default function AuthProvider({children}){
     // Put some state in the context
 
     const [number, setNumber] = useState(10)
+    setInterval( () => setNumber(number + 1), 10000)
 
     return(
 
