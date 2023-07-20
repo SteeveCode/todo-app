0c8442449a440f9c2ffeedf3edfed416cc0671bf
steevecode <stevecode@yahoo.com>
2023-07-20 15:26:01 +0100

    Section 04: Step 04 - Invoking Spring Boot Hello World Bean and Path Param REST API from React

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { useState } from 'react'
 import {useParams, Link} from 'react-router-dom'
 
 
@@ -7,20 +8,27 @@ function WelcomeComponent() {
 
     const {username} = useParams()
 
+    const [message, setMessage] = useState(null)
+
     function callHelloWorldRestApi(){
 
         console.log('called')
-       
 
-        axios.get('http://localhost:8080/hello-world')
+        // axios.get('http://localhost:8080/hello-world')
+        // .then( (response) => successfulResponse(response) )
+        // .catch ( (error) => errorResponse(error) )
+        // .finally ( () => console.log('cleanup') )
+
+        axios.get('http://localhost:8080/hello-world-bean')
         .then( (response) => successfulResponse(response) )
         .catch ( (error) => errorResponse(error) )
         .finally ( () => console.log('cleanup') )
-
     }
-
+   
     function successfulResponse(response) {
         console.log(response)
+        // setMessage(response.data)
+        setMessage(response.data.message)
       
     }
 
@@ -35,9 +43,10 @@ function WelcomeComponent() {
                 Manage your todos - <Link to="/todos">Go here</Link>
             </div>
             <div>
-            <button className="btn btn-success m-5" onClick={callHelloWorldRestApi}>
-                    Call Hello World</button>
-            </div>
+            <button className="btn btn-success m-5" onClick={callHelloWorldRestApi}>Call Hello World</button>
+            </div> 
+            <div className="text-info">{message}</div>
+           
         </div>
     )
 }
