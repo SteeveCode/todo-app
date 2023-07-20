01532cbb3bcd4db7152495590c2196c8963203a3
steevecode <stevecode@yahoo.com>
2023-07-20 12:31:58 +0100

    Section 04: Step 02 - Calling Spring Boot Hello World REST API from React Hello World Component

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import {useParams, Link} from 'react-router-dom'
 
 
@@ -6,11 +7,36 @@ function WelcomeComponent() {
 
     const {username} = useParams()
 
+    function callHelloWorldRestApi(){
+
+        console.log('called')
+       
+
+        axios.get('http://localhost:8080/hello-world')
+        .then( (response) => successfulResponse(response) )
+        .catch ( (error) => errorResponse(error) )
+        .finally ( () => console.log('cleanup') )
+
+    }
+
+    function successfulResponse(response) {
+        console.log(response)
+      
+    }
+
+    function errorResponse(error) {
+        console.log(error)
+    }
+
     return (
         <div className="WelcomeComponent">
             <h1>Welcome {username}</h1>
             <div>
                 Manage your todos - <Link to="/todos">Go here</Link>
+            </div>
+            <div>
+            <button className="btn btn-success m-5" onClick={callHelloWorldRestApi}>
+                    Call Hello World</button>
             </div>
         </div>
     )
