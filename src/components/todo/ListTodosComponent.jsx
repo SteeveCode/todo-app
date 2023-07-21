007eefe14cee60b7149b0e721c531374e6d3504d
steevecode <stevecode@yahoo.com>
2023-07-21 09:52:09 +0100

    Section 04: Step 10 - Adding Delete Feature to React Frontend

--- src/components/todo/ListTodosComponent.jsx
+++ src/components/todo/ListTodosComponent.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { retrieveAllTodosForUsername } from "./api/TodoApiService"
+import { deleteTodoApi, retrieveAllTodosForUsernameApi } from "./api/TodoApiService"
 
 function ListTodosComponent() {
     const today = new Date()
@@ -7,22 +7,31 @@ function ListTodosComponent() {
     const targetDate = new Date(today.getFullYear()+12, today.getMonth(), today.getDay())
 
     const [todos, setTodos] = useState([])
-    
-    // const todos = [
-    //                 // {id: 1, description: 'Learn AWS', done: false, targetDate:targetDate},
-    //                 // {id: 2, description: 'Learn Full Stack Dev', done: false, targetDate:targetDate},
-    //                 // {id: 3, description: 'Learn DevOps', done: false, targetDate:targetDate},
-    //             ]
+    const [message, setMessage] = useState(null)
 
-    useEffect(
-        () => refreshTodo(), []
-    )
+   // useEffect - tell React that your component needs to do something after render
+    useEffect( () => refreshTodo(), [] )
     
     function refreshTodo(){
-        retrieveAllTodosForUsername('in28ms')
+        retrieveAllTodosForUsernameApi('in28ms')
         .then(response => {
             setTodos(response.data)
         })
+        .catch(error => console.log(error))
+    }
+
+    function deleteTodo(id){
+        console.log('clicked ' + id)
+        deleteTodoApi('in28ms', id)
+        .then(
+            () => {
+                setMessage(`Delete of Todo with id = ${id} successful`)
+                refreshTodo()
+
+            }
+            //1: Display a message
+            //2: Update Todos list
+        )
         .catch(error => console.log(error))
     }
 
@@ -30,14 +39,18 @@ function ListTodosComponent() {
                 return (
                     <div className="container">
                         <h1>Things You Want To Do!</h1>
+
+                        {message && <div className="alert alert-warning">{message}</div> }
+                        
                         <div>
                             <table className='table'>
                                 <thead>
                                         <tr>
-                                            <td>ID</td>
-                                            <td>Description</td>
-                                            <td>Is Done?</td>
-                                            <td>Target Date</td>
+                                            
+                                            <th>Description</th>
+                                            <th>Is Done?</th>
+                                            <th>Target Date</th>
+                                            <th>Delete</th>
                                         </tr>
                                 </thead>
                                 <tbody>
@@ -45,11 +58,11 @@ function ListTodosComponent() {
                                     todos.map(
                                         todo => (
                                             <tr key={todo.id}>
-                                                <td>{todo.id}</td>
                                                 <td>{todo.description}</td>
                                                 <td>{todo.done.toString()}</td>
                                                 {/* <td>{todo.targetDate.toDateString()}</td> */}
                                                 <td>{todo.targetDate.toString()}</td>
+                                                <td><button className="btn btn-warning" onClick={ () => deleteTodo(todo.id)}>Delete</button></td>
                                             </tr>
                                         )
                                     )
--- src/components/todo/api/TodoApiService.js
+++ src/components/todo/api/TodoApiService.js
@@ -5,6 +5,9 @@ const apiClient = axios.create(
     baseURL: 'http://localhost:8080'
 }); 
 
-export const retrieveAllTodosForUsername = (username) => apiClient.get(`/users/${username}/todos`)
+export const retrieveAllTodosForUsernameApi = (username) => apiClient.get(`/users/${username}/todos`)
 // http://localhost:8080/users/in28ms/todos
+
+export const deleteTodoApi = (username, id) => apiClient.delete(`/users/${username}/todos/${id}`)
+
 
